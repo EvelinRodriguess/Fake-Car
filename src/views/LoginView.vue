ae73ccbba18d138c84c85ba8dd39fb6340265949
EvelinRodriguess <evelinrodrigues.ifc@gmail.com>
2024-07-10 16:54:06 -0300

    login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+
+</script>
+
+<template>
+    <h1>Login</h1>
+    <div class="authContainer">
+    <passage-auth
+      app-id="uO6bcSXG1Ocr01wnGAT6raVL"
+    ></passage-auth>
+  </div>
+</template>
