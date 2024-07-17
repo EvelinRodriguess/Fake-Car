7fb903bee52a19953f42c4bee1512da7ff8756ee
EvelinRodriguess <evelinrodrigues.ifc@gmail.com>
2024-07-17 16:44:29 -0300

    aula 7

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dns21f04dhg"
+    "revision": "0.jhk6co22asg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
